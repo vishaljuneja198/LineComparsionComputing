e581a8f7c3c3c01809f9b5ff8f15ea3e6ec3c65c
vishaljuneja198 <vishaljuneja198@gmail.com>
2022-05-26 04:26:03 +0530

    length of line Sqt formula

--- LineProblem.java
+++ LineProblem.java
@@ -1,4 +1,3 @@
-//Welcome message program
 package com.bridgelabz;
 import java.lang.Math;
 
@@ -6,6 +5,10 @@ public class LineProblem {
     public static void main(String[] args) {
         System.out.println("Welcome to Line Comparison Computation Program on Master Branch");
 
+        double x1 = 80.0, x2 = 26.0, y1 = 36.0, y2 = 87.0;
+        double a = Math.sqrt(x2-x1);
+        double b = Math.sqrt(y2-y1);
 
+        System.out.println(a + " " + b);
     }
 }
