021622740e1820a2f3767a2880ca74537be2bd6c
vishaljuneja198 <vishaljuneja198@gmail.com>
2022-05-26 04:19:10 +0530

    Welcome to comparsion computing program

---
+++ LineProblem.java
@@ -0,0 +1,11 @@
+//Welcome message program
+package com.bridgelabz;
+import java.lang.Math;
+
+public class LineProblem {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Line Comparison Computation Program on Master Branch");
+
+
+    }
+}
